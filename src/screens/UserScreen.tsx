35ddfd7ed9f2e5ab889b98f239e7443c8198f6cf
ikarosuu <icaro.code@proton.me>
2025-09-10 17:38:01 -0400

    criando homescreen

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
+import { useState } from 'react'
+import { StackNavigationProp } from '@react-navigation/stack'
 
 export default function UserScreen(){
+    const [inputText, setinputText] = useState("")
+
+    const navigateToUserScreen = () => {
+
+    }
+    
     return(
         <View>
-            <Text>Works!</Text>
+            <TextInput 
+            placeholder="Digite seu nome..." 
+            value={inputText}
+            onChangeText={(text)=> setinputText(text)}
+        />
+        <Button title="Logar" onPress={navigateToUserScreen}/>
         </View>
     )
 }
