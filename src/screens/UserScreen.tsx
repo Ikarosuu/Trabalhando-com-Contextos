ebbfd7684d8ab4b79a8012c704d42bcef14e3ab0
ikarosuu <icaro.code@proton.me>
2025-09-11 12:09:01 -0400

    usando um contexto

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,6 +1,9 @@
 import { View, Text } from 'react-native';
 import { RouteProp } from '@react-navigation/native'
 import { RootStackParamList } from '../../App';
+//usando contexto
+import { useContext } from 'react'
+import { UserContext } from '../contexts/userContext';
 
 type UserScreenProps = {
     route: RouteProp<RootStackParamList, 'User'>
@@ -8,6 +11,10 @@ type UserScreenProps = {
 
 export default function UserScreen({route}: UserScreenProps) {
     const { username } = route.params
+
+    // usando contexto
+    const UserContextValue = useContext(UserContext)
+    const nome = UserContextValue.nome
 
     return (
         <View>
